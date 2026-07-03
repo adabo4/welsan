662080bc87775e61a6218cd71e76694c5545ff69
adabo4 <adabo4@gmail.com>
2026-07-03 20:57:40 +0200

    changed style of nav and header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,20 +6,48 @@ import styles from "./header.module.css";
 
 export default function Header() {
   return (
+    // <header className={styles.headerWrapper}>
+    //   <Image
+    //     src={hero}
+    //     width={650}
+    //     height={500}
+    //     alt="hero"
+    //     className={styles.hero}
+    //   ></Image>
+    //   <div className={styles.headerText}>
+    //     <h1 className={styles.title}>
+    //       <span className={styles.individual}>Individuálne</span>{" "}
+    //       <span className={styles.online}>online hodiny </span>
+    //       <span className={styles.english}>Angličtiny</span>
+    //     </h1>
+    //     <div className={styles.subtitleWrapper}>
+    //       <h2>Angličtina bez stresu.</h2>
+    //       <h2>
+    //         Vaším tempom. Podľa{" "}
+    //         <span className={styles.goals}>vašich cieľov.</span>
+    //       </h2>
+    //     </div>
+    //   </div>
+    // </header>
     <header className={styles.headerWrapper}>
-      <Image
-        src={hero}
-        width={650}
-        height={500}
-        alt="hero"
-        className={styles.hero}
-      ></Image>
+      <div className={styles.heroWrapper}>
+        <Image
+          src={hero}
+          width={650}
+          height={500}
+          alt="hero"
+          className={styles.hero}
+          priority
+        />
+      </div>
+
       <div className={styles.headerText}>
         <h1 className={styles.title}>
-          <span className={styles.individual}>Individuálne</span>{" "}
-          <span className={styles.online}>online hodiny </span>
+          <span className={styles.individual}>Individuálne</span>
+          <span className={styles.online}>online hodiny</span>
           <span className={styles.english}>Angličtiny</span>
         </h1>
+
         <div className={styles.subtitleWrapper}>
           <h2>Angličtina bez stresu.</h2>
           <h2>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -13,8 +13,8 @@ export default function Navbar() {
           <Image
             className={styles.logo}
             src={logo}
-            width={190}
-            height={130}
+            width={100}
+            height={60}
             alt="logo"
           ></Image>
           <p className={styles.logoText}>Vzdelávanie, ktoré dáva zmysel</p>
@@ -22,16 +22,16 @@ export default function Navbar() {
         <Image
           alt="sraf"
           src={sraf}
-          width={180}
-          height={180}
+          width={90}
+          height={90}
           className={styles.sraf}
         ></Image>
 
         <div className={styles.flagWrapper}>
           <Image
             src={flag}
-            width={400}
-            height={150}
+            width={250}
+            height={80}
             alt="flag"
             className={styles.flagImg}
           ></Image>
