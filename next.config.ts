f7b9b15efc23bd81a300a50f81fd67f5195dcb77
adabo4 <adabo4@gmail.com>
2026-07-05 10:52:20 +0200

    changed note size on mobile

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  allowedDevOrigins: ["10.5.0.2"],
 };
 
 export default nextConfig;
